dfd4d6c38dc75575aa840b6629b53206fb96a8b7
mjdoiron <mjdoiron@gmail.com>
2017-09-12 02:11:15 -0400

    Added Allow Cross Domain for iFrames

--- app.js
+++ app.js
@@ -34,10 +34,26 @@ const experimentController = require('./controllers/experiment');
  */
 const passportConfig = require('./config/passport');
 
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 /**
  * Create Express server.
  */
 const app = express();
+
+app.use(allowCrossDomain);
 
 /**
  * Connect to MongoDB.
