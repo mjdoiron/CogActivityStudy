42f254d0b3297f89458507286266a13d417d4ae9
mjdoiron <mjdoiron@gmail.com>
2017-09-12 02:17:21 -0400

    Trying different CORS function

--- app.js
+++ app.js
@@ -35,26 +35,17 @@ const experimentController = require('./controllers/experiment');
 const passportConfig = require('./config/passport');
 
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
 /**
  * Create Express server.
  */
 const app = express();
 
-app.use(allowCrossDomain);
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 /**
  * Connect to MongoDB.
  */
